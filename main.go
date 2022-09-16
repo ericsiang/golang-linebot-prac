151d5856a45f7129d1d6185d9020947997048384
ericsiang <42998157+ericsiang@users.noreply.github.com>
2022-09-17 02:17:12 +0800

    #3 setup necessary config of LINE, MongoDB

--- main.go
+++ main.go
@@ -5,9 +5,33 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
 	"homework/command"
 )
 
+var config *viper.Viper
+
 func main() {
 	command.DockerUp()
+	config := initConfigure()
+	fmt.Println(config)
+}
+
+func initConfigure() *viper.Viper {
+	config := viper.New()
+	config.SetConfigName("config")
+	config.SetConfigType("yaml")
+	config.AddConfigPath("./config")
+	if err := config.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	config.WatchConfig()
+	config.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
+
+	return config
 }
