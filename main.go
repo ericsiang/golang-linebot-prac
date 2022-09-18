10a92104063a2d75eb8c6f938b4ce55c6bdb1d79
ericsiang <42998157+ericsiang@users.noreply.github.com>
2022-09-18 16:32:33 +0800

    #5 Create a Gin API receive message from line webhook, save the user info and message in MongoDB

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ package main
 import (
 	"fmt"
 	"github.com/fsnotify/fsnotify"
+	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"homework/command"
+	"homework/models"
 	"homework/mongodb"
 	"log"
+	"time"
 )
 
 var config *viper.Viper
@@ -18,13 +24,116 @@ var config *viper.Viper
 func main() {
 	command.DockerUp()
 	config := initConfigure()
-	dsn := fmt.Sprintf("mongodb://%s:%s@%s:%d", config.Get("database.user"), config.Get("database.password"), config.Get("database.host"), config.Get("database.port"))
-	client, ctx, err := mongodb.ConnectMongoDb(dsn)
-
+	bot, err := linebot.New(fmt.Sprintf("%s", config.Get("linebot.channelSecret")), fmt.Sprintf("%s", config.Get("linebot.channelAccessToken")))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(client, ctx)
+
+	router := gin.Default()
+	router.POST("/callback", func(c *gin.Context) {
+		events, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				fmt.Println("ErrInvalidSignature")
+				log.Print(err)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				dsn := fmt.Sprintf("mongodb://%s:%s@%s:%d", config.Get("database.user"), config.Get("database.password"), config.Get("database.host"), config.Get("database.port"))
+				client, ctx, err := mongodb.ConnectMongoDb(dsn)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+				messageType := ""
+				messageText := ""
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					messageType = "text"
+					messageText = message.Text
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已收到文字訊息！")).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.ImageMessage:
+					messageType = "image"
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已收到圖片訊息！")).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.VideoMessage:
+					messageType = "video"
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已收到影片訊息！")).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.AudioMessage:
+					messageType = "audio"
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已收到語音訊息！")).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.FileMessage:
+					messageType = "file"
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已收到檔案訊息！")).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.LocationMessage:
+					messageType = "location"
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已收到定位訊息！")).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.StickerMessage:
+					messageType = "sticker"
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已收到貼圖訊息！")).Do(); err != nil {
+						log.Print(err)
+					}
+				default:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已收到訊息！")).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+
+				userCollection := mongodb.GetCollection(client, "users")
+				userCount, err := userCollection.CountDocuments(ctx, bson.M{"userid": event.Source.UserID})
+				if err != nil {
+					log.Fatal(userCount)
+				}
+				if userCount == 0 {
+					user := models.User{
+						Id:        primitive.NewObjectID(),
+						UserId:    event.Source.UserID,
+						CreatedAt: time.Now(),
+						UpdatedAt: time.Now(),
+					}
+					result, err := userCollection.InsertOne(ctx, user)
+					if err != nil {
+						log.Print(err)
+					}
+					fmt.Println(result)
+				}
+
+				lineMessageCollection := mongodb.GetCollection(client, "lineMessage")
+				lineMessage := models.LineMessage{
+					Id:             primitive.NewObjectID(),
+					EventType:      "message",
+					MessageType:    messageType,
+					MessageText:    messageText,
+					UserId:         event.Source.UserID,
+					ReplyToken:     event.ReplyToken,
+					WebhookEventId: event.WebhookEventID,
+					CreatedAt:      time.Now(),
+					UpdatedAt:      time.Now(),
+				}
+				result, err := lineMessageCollection.InsertOne(ctx, lineMessage)
+				if err != nil {
+					log.Print(err)
+				}
+				fmt.Println(result)
+			}
+		}
+
+	})
+
+	router.Run(":80")
 }
 
 func initConfigure() *viper.Viper {
